4cf1c2e9457c9e5725ad0f2d2e00a0dae8e81e81
pra8953 <154659571+pra8953@users.noreply.github.com>
2024-07-09 13:49:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-function Msg1(){
-    alert('add to bag  in js');
-}
-function Msg2(){
-    alert('add to wishlist');
-}
-
-let cartQuantity=0;
-document.querySelector('h1').innerText=`Your bag has ${cartQuantity} items`;
-
-let myName="Prashant Kumar ji";
-document.querySelector('#name').innerHTML=`My name is ${myName}`;
 
