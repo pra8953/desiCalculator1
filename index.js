7fbad221b8a8eac7970f69b476bbd6f567cc4cbd
pra8953 <154659571+pra8953@users.noreply.github.com>
2024-07-09 13:43:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function Msg1(){
+    alert('add to bag  in js');
+}
+function Msg2(){
+    alert('add to wishlist');
+}
+
+let cartQuantity=0;
+document.querySelector('h1').innerText=`Your bag has ${cartQuantity} items`;
+
+let myName="Prashant Kumar ji";
+document.querySelector('#name').innerHTML=`My name is ${myName}`;
+
